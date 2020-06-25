8d137908c3c5e68c23dd7749b8cb263ff78cc72f
artytrik <tema-luch@mail.ru>
2020-06-25 10:40:42 +0300

    Добавляет начальное состояение проекта

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://rinoknn.ru/admin/');
+define('HTTP_CATALOG', 'http://rinoknn.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://rinoknn.ru/admin/');
+define('HTTPS_CATALOG', 'http://rinoknn.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/u0776125/public_html/rinoknn/admin/');
+define('DIR_SYSTEM', '/var/www/u0776125/public_html/rinoknn/system/');
+define('DIR_IMAGE', '/var/www/u0776125/public_html/rinoknn/image/');
+define('DIR_STORAGE', '/var/www/u0776125/rinoknn/storage/');
+define('DIR_CATALOG', '/var/www/u0776125/public_html/rinoknn/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'u0776125_rinoknn');
+define('DB_PASSWORD', 'S]1J(7pZ7B');
+define('DB_DATABASE', 'u0776125_rinoknn');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://rinoknn.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://rinoknn.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/u0776125/public_html/rinoknn/catalog/');
+define('DIR_SYSTEM', '/var/www/u0776125/public_html/rinoknn/system/');
+define('DIR_IMAGE', '/var/www/u0776125/public_html/rinoknn/image/');
+define('DIR_STORAGE', '/var/www/u0776125/rinoknn/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'u0776125_rinoknn');
+define('DB_PASSWORD', 'S]1J(7pZ7B');
+define('DB_DATABASE', 'u0776125_rinoknn');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
