ed53a96b1f3f8d451d30c6b83be39d625faaccc4
artytrik <tema-luch@mail.ru>
2020-08-05 16:54:44 +0300

    Завершает перевод

--- catalog/language/en-gb/account/edit.php
+++ catalog/language/en-gb/account/edit.php
@@ -1,23 +1,24 @@
 <?php
 // Heading
-$_['heading_title']      = 'My Account Information';
+$_['heading_title']      = 'Учетная запись';
 
 // Text
-$_['text_account']       = 'Account';
-$_['text_edit']          = 'Edit Information';
-$_['text_your_details']  = 'Your Personal Details';
-$_['text_success']       = 'Success: Your account has been successfully updated.';
+$_['text_account']       = 'Личный Кабинет';
+$_['text_edit']          = 'Редактировать информацию';
+$_['text_your_details']  = 'Ваша учетная запись';
+$_['text_success']       = 'Ваша учетная запись была успешно обновлена!';
 
 // Entry
-$_['entry_firstname']    = 'First Name';
-$_['entry_lastname']     = 'Last Name';
+$_['entry_firstname']    = 'Имя, Отчество';
+$_['entry_lastname']     = 'Фамилия';
 $_['entry_email']        = 'E-Mail';
-$_['entry_telephone']    = 'Telephone';
+$_['entry_telephone']    = 'Телефон';
 
 // Error
-$_['error_exists']       = 'Warning: E-Mail address is already registered!';
-$_['error_firstname']    = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']     = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']        = 'E-Mail Address does not appear to be valid!';
-$_['error_telephone']    = 'Telephone must be between 3 and 32 characters!';
-$_['error_custom_field'] = '%s required!';
+$_['error_exists']       = 'Данный E-Mail уже зарегистрирован!';
+$_['error_firstname']    = 'Имя должно быть от 1 до 32 символов!';
+$_['error_lastname']     = 'Фамилия должна быть от 1 до 32 символов!';
+$_['error_email']        = 'E-Mail адрес введен неверно!';
+$_['error_telephone']    = 'Номер телефона должен быть от 3 до 32 символов!';
+$_['error_custom_field'] = '%s необходим!';
+
--- catalog/language/en-gb/account/forgotten.php
+++ catalog/language/en-gb/account/forgotten.php
@@ -1,21 +1,22 @@
 <?php
 // Heading
-$_['heading_title']   = 'Forgot Your Password?';
+$_['heading_title']   = 'Забыли пароль?';
 
 // Text
-$_['text_account']    = 'Account';
-$_['text_forgotten']  = 'Forgotten Password';
-$_['text_your_email'] = 'Your E-Mail Address';
-$_['text_email']      = 'Enter the e-mail address associated with your account. Click submit to have a password reset link e-mailed to you.';
-$_['text_success']    = 'An email with a confirmation link has been sent your email address.';
+$_['text_account']    = 'Личный Кабинет';
+$_['text_forgotten']  = 'Забытый пароль?';
+$_['text_your_email'] = 'Ваш E-Mail';
+$_['text_email']      = 'Введите E-Mail Вашей учетной записи. Нажмите кнопку Вперед, чтобы получить пароль по электронной почте.';
+$_['text_success']    = 'Новый пароль был выслан на Ваш E-Mail.';
 
 // Entry
-$_['entry_email']     = 'E-Mail Address';
-$_['entry_password']  = 'New Password';
-$_['entry_confirm']   = 'Confirm';
+$_['entry_email']     = 'E-Mail';
+$_['entry_password']  = 'Новый пароль';
+$_['entry_confirm']   = 'Подтвердите новый пароль';
 
 // Error
-$_['error_email']     = 'Warning: The E-Mail Address was not found in our records, please try again!';
-$_['error_approved']  = 'Warning: Your account requires approval before you can login.';
-$_['error_password']  = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']   = 'Password and password confirmation do not match!';
+$_['error_email']     = 'E-Mail адрес не найден, проверьте и попробуйте еще раз!';
+$_['error_approved']  = 'Внимание! Ваш аккаунт еще не активирован.';
+$_['error_password']  = 'Пароль должен быть от 4 до 20 символов!';
+$_['error_confirm']   = 'Пароль и подтверждение пароля различны!';
+
